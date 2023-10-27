44f3471d78ad79f343ea0c979f42d990e384f9c0
ewurapoms <mzpomaa35@gmail.com>
2023-10-27 07:18:31 -0700

    task 1 debugging

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
-# Installs flask version 2.1.0
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Installs flask version 2.1.0 and werkzeug 2.1.1
+
+package {'werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3',
+}
+
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
