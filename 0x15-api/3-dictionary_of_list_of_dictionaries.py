5d24494dafa0c299e4df51fb5fb9986ed3ceb272
ewurapoms <mzpomaa35@gmail.com>
2023-11-27 04:13:57 -0800

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Exports employee data to JSON format"""
+import requests
+import sys
+import json
+
+
+def todo_json():
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    task_list = {}
+
+    for user in users:
+        user_id = user['id']
+        username = user['username']
+
+        todos = requests.get(url + "todos", params={"userId": user_id}).json()
+        user_tasks = [
+            {
+                "username": username,
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            for task in todos
+        ]
+        task_list[user_id] = user_tasks
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(task_list, json_file, indent=2)
+
+
+if __name__ == "__main__":
+    todo_json()
