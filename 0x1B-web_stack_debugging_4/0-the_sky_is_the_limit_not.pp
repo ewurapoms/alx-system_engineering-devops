9981816f67f31ca1f01602d4a807aefa23ceab92
ewurapoms <mzpomaa35@gmail.com>
2023-12-20 22:28:18 -0800

    updating terminal variable to path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Enable the user holberton to login and open files without error
 
 exec {'replace':
-  terminal => ['/bin', '/usr/bin'],
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+  path    => ['/bin', '/usr/bin'],
+  command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before  => Exec['restart'],
 }
 
 exec {'restart':
-  terminal => ['/bin', '/usr/bin'],
-  command  => 'sudo service nginx restart',
+  path    => ['/bin', '/usr/bin'],
+  command => 'sudo service nginx restart',
 }
