000803e7c4af01f37931005544c443eead13c950
ewurapoms <mzpomaa35@gmail.com>
2023-12-20 22:24:35 -0800

    updating terminal variable

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Enable the user holberton to login and open files without error
 
 exec {'replace':
-  terminal => shell,
+  terminal => ['/bin', '/usr/bin'],
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   before   => Exec['restart'],
 }
 
 exec {'restart':
-  terminal => shell,
+  terminal => ['/bin', '/usr/bin'],
   command  => 'sudo service nginx restart',
 }
